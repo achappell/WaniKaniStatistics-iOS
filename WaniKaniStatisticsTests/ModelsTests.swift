43593cf77b23a8ce7ea13894a2a1db6e93b7dc05
Amanda Chappell <achappell@me.com>
2018-04-29 11:46:14 -0500

    more tests

--- WaniKaniStatisticsTests/ModelsTests.swift
+++ WaniKaniStatisticsTests/ModelsTests.swift
@@ -21,9 +21,29 @@ class ModelsTests: XCTestCase {
         super.tearDown()
     }
     
-    func testLesson() {
+    func testLessonInit() {
         let lesson = Lesson(subject_ids: [1])
         XCTAssertEqual(lesson.subject_ids.first, 1)
+    }
+    
+    func testLevelProgressionInit() {
+        let levelProgression = LevelProgression(level: 1, started_at: "2018-04-29T16:00:00.000000Z", passed_at: "2018-04-29T16:00:00.000000Z", abandoned_at: "2018-04-29T16:00:00.000000Z")
+        XCTAssertEqual(levelProgression.level, 1)
+        XCTAssertEqual(levelProgression.started_at, "2018-04-29T16:00:00.000000Z")
+        XCTAssertEqual(levelProgression.passed_at, "2018-04-29T16:00:00.000000Z")
+        XCTAssertEqual(levelProgression.abandoned_at, "2018-04-29T16:00:00.000000Z")
+    }
+    
+    func testLevelProgressionStartedAt() {
+        let levelProgression = LevelProgression(level: 1, started_at: "2018-04-29T16:00:00.000000Z", passed_at: "2018-04-29T16:00:00.000000Z", abandoned_at: "2018-04-29T16:00:00.000000Z")
+        
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        
+        XCTAssertEqual(levelProgression.startedAt(),formatter.date(from: "2018-04-29T16:00:00.000000Z"))
     }
     
 }
